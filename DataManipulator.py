4b5f148428368f8c182cce7cec777ed1716ecb5e
Camilo <Videscamilo@yahoo.com>
2018-11-08 18:53:30 -0500

    equisde 4: started trying to implement dt and nb

--- DataManipulator.py
+++ DataManipulator.py
@@ -1,3 +1,6 @@
+from sklearn import tree
+from sklearn import naive_bayes
+
 #Read the the Information Label DS1
 with open('ds1/ds1Info.csv', 'r') as file:
     infoData = [line.split(',') for line in file.read().split('\n')][1:]
@@ -8,13 +11,16 @@ infoLabels = [d[-1] for d in infoData]
 #Read the the training set DS1
 with open('ds1/ds1Train.csv', 'r') as file:
     trainData = [line.split(',') for line in file.read().split('\n')]
-trainFeatures = [[element for element in row] for row in trainData]
-
+trainData = [[element for element in row] for row in trainData]
+trainFeatures = [d[:-1] for d in trainData]
+trainLabels = [d[-1] for d in trainData]
 
 #Read the the validation set DS1
 with open('ds1/ds1Val.csv', 'r') as file:
     validationData = [line.split(',') for line in file.read().split('\n')]
 validationFeatures = [[element for element in row] for row in validationData]
+validationFeatures = [d[:-1] for d in validationData]
+validationLabels = [d[-1] for d in validationData]
 
 '''
 #Prints all the labels in a row.
@@ -46,10 +52,27 @@ print("Info label  0: ", infoLabels[0])
 
 print('\n')
 
-print("Training data row 1: ", trainData[0])
-print("Training data row 1960: ", trainData[1959])
+print("Training feature row 1: ", trainFeatures[0])
+print("Training label  0: ", trainLabels[0])
+print("Training data row 1960: ", trainFeatures[1959])
+print("Training label  1960: ", trainLabels[1959])
 print('\n')
 
 print("Validation feature row 1: ", validationFeatures[0])
+print("Validation label row 1: ", validationLabels[0])
 print("Validation feature row 514: ", validationFeatures[513])
+print("Validation label row 514: ", validationLabels[513])
 print('\n')
+
+
+#Decision Tree Implementation
+#classifier = tree.DecisionTreeClassifier()
+#classifier.fit(trainFeatures, trainLabels)
+#validationPredicted = classifier.predict(validationFeatures)
+
+
+#Naive Bayes
+classifier = naive_bayes.MultinomialNB()
+#classifier.fit(trainFeatures, trainLabels)
+#validation_predicted = classifier.predict(validationFeatures)
+
