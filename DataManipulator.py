e8d20395a3f2c1b25ad50c912d7a95e20783dcb5
Camilo <Videscamilo@yahoo.com>
2018-11-10 11:37:18 -0500

    equisde 7 Added Linear regression for ds1 and ds2

--- DataManipulator.py
+++ DataManipulator.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn import tree
 from sklearn import naive_bayes
+from sklearn import linear_model
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report, confusion_matrix
 
@@ -25,49 +26,6 @@ validationData = [[int(element) for element in row] for row in validationData]
 validationFeatures = [d[:-1] for d in validationData]
 validationLabels = [d[-1] for d in validationData]
 
-'''
-#Prints all the labels in a row.
-print("All labels")
-for row in infoLabels:
-    print(row)
-    
-#Prints all the labels in InfoData.
-print("All Training data")
-for row in trainData:
-    print(row)
-    
-#Prints all the labels in InfoData.
-print("All validation data")
-for row in validationData:
-    print(row)
-'''
-'''
-#Prints all the labels (index/character) in InfoData.
-print("All Labels")
-for row in infoData:
-    print(row)
-print('\n')
-
-#Row 0 of each array of each file
-print("Info data row 0:", (infoData[0]))
-print("Info feature 0: ", infoFeatures[0])
-print("Info label  0: ", infoLabels[0])
-
-print('\n')
-
-print("Training feature row 1: ", trainFeatures[0])
-print("Training label  0: ", trainLabels[0])
-print("Training data row 1960: ", trainFeatures[1959])
-print("Training label  1960: ", trainLabels[1959])
-print('\n')
-'''
-'''
-print("Validation feature row 1: ", validationFeatures[0])
-print("Validation label row 1: ", validationLabels[0])
-print("Validation feature row 514: ", validationFeatures[513])
-print("Validation label row 514: ", validationLabels[513])
-print('\n')
-'''
 
 #Parsing the data to arrays
 trainFeaturesArray = np.asarray(trainFeatures)
@@ -80,14 +38,14 @@ classifier.fit(trainFeaturesArray, trainLabelsArray)
 validationPredicted = classifier.predict(validationFeaturesArray)
 validationPredictedArray = np.asarray(validationPredicted)
 
-print(confusion_matrix(validationLabels, validationPredictedArray))
-print(classification_report(validationLabels, validationPredictedArray))
+#print(confusion_matrix(validationLabels, validationPredictedArray))
+#print(classification_report(validationLabels, validationPredictedArray))
 
 #Decision Tree Predicted Labels
-print('Predicted Labels for decision Tree')
-print(validationPredictedArray)
+print('Predicted Labels for decision Tree for DS1')
+#print(validationPredictedArray)
 accuracy = accuracy_score(validationLabels, validationPredictedArray)
-print('Accuracy of Decision Tree Classifier: ', "{0:.3%}".format(accuracy))
+print('Accuracy of Decision Tree Classifier for DS1: ', "{0:.3%}".format(accuracy))
 print('\n')
 
 with open('ds1Val-dt.csv', 'w') as file:
@@ -102,80 +60,35 @@ validationPredicted = classifier.predict(validationFeaturesArray)
 validationPredictedArray = np.asarray(validationPredicted)
 
 #Predicted Naive Bayes Labels
-print('Predicted Labels for Naive Bayes')
-print(validationPredictedArray)
+print('Predicted Labels for Naive Bayes for DS1')
+#print(validationPredictedArray)
 accuracy = accuracy_score(validationLabels, validationPredictedArray)
-print('Accuracy of Naive Bayes Classifier: ', "{0:.3%}".format(accuracy))
+print('Accuracy of Naive Bayes Classifier for DS1: ', "{0:.3%}".format(accuracy))
 print('\n')
 
 with open('ds1Val-nb.csv', 'w') as file:
  for i in range(len(validationPredictedArray)):
     file.write('%d,%d\n' % (i + 1, validationPredictedArray[i]))
 
-# Read the the Information Label DS1
-with open('ds1/ds1Info.csv', 'r') as file:
-    infoData = [line.split(',') for line in file.read().split('\n')][1:]
-infoData = [[element for element in row] for row in infoData]
-infoFeatures = [d[:-1] for d in infoData]
-infoLabels = [d[-1] for d in infoData]
 
-# Read the the training set DS1
-with open('ds1/ds1Train.csv', 'r') as file:
-    trainData = [line.split(',') for line in file.read().split('\n')]
-trainData = [[int(element) for element in row] for row in trainData]
-trainFeatures = [d[:-1] for d in trainData]
-trainLabels = [d[-1] for d in trainData]
+#Linear Model Implementation
+classifier = linear_model.LinearRegression()
+classifier.fit(trainFeaturesArray, trainLabelsArray)
+validationPredicted = classifier.predict(validationFeaturesArray)
+validationPredictedArray = np.asarray(validationPredicted)
 
-# Read the the validation set DS1
-with open('ds1/ds1Val.csv', 'r') as file:
-    validationData = [line.split(',') for line in file.read().split('\n')]
-validationData = [[int(element) for element in row] for row in validationData]
-validationFeatures = [d[:-1] for d in validationData]
-validationLabels = [d[-1] for d in validationData]
-
-'''
-#Prints all the labels in a row.
-print("All labels")
-for row in infoLabels:
-    print(row)
-
-#Prints all the labels in InfoData.
-print("All Training data")
-for row in trainData:
-    print(row)
-
-#Prints all the labels in InfoData.
-print("All validation data")
-for row in validationData:
-    print(row)
-'''
-'''
-#Prints all the labels (index/character) in InfoData.
-print("All Labels")
-for row in infoData:
-    print(row)
+#Predicted Linear Model Labels
+print('Predicted Labels for Linear Model for DS1')
+#print(validationPredictedArray)
+print('Accuracy of Linear Model Classifier for DS1: ', "{0:.3%}".format(accuracy))
 print('\n')
 
-#Row 0 of each array of each file
-print("Info data row 0:", (infoData[0]))
-print("Info feature 0: ", infoFeatures[0])
-print("Info label  0: ", infoLabels[0])
+with open('ds1Val-lm.csv', 'w') as file:
+ for i in range(len(validationPredictedArray)):
+    file.write('%d,%d\n' % (i + 1, validationPredictedArray[i]))
 
-print('\n')
 
-print("Training feature row 1: ", trainFeatures[0])
-print("Training label  0: ", trainLabels[0])
-print("Training data row 1960: ", trainFeatures[1959])
-print("Training label  1960: ", trainLabels[1959])
-print('\n')
-'''
-'''
-print("Validation feature row 1: ", validationFeatures[0])
-print("Validation label row 1: ", validationLabels[0])
-print("Validation feature row 514: ", validationFeatures[513])
-print("Validation label row 514: ", validationLabels[513])
-print('\n')
-'''
+#################################################################################
 ##DS2
 #Read the the Information Label DS2
 with open('ds2/ds2Info.csv', 'r') as file:
@@ -209,14 +122,14 @@ classifier.fit(trainFeaturesArray, trainLabelsArray)
 validationPredicted = classifier.predict(validationFeaturesArray)
 validationPredictedArray = np.asarray(validationPredicted)
 
-print(confusion_matrix(validationLabels, validationPredictedArray))
-print(classification_report(validationLabels, validationPredictedArray))
+#print(confusion_matrix(validationLabels, validationPredictedArray))
+#print(classification_report(validationLabels, validationPredictedArray))
 
 # Decision Tree Predicted Labels
-print('Predicted Labels for decision Tree')
-print(validationPredictedArray)
+print('Predicted Labels for decision Tree for DS2')
+#print(validationPredictedArray)
 accuracy = accuracy_score(validationLabels, validationPredictedArray)
-print('Accuracy of Decision Tree Classifier: ', "{0:.3%}".format(accuracy))
+print('Accuracy of Decision Tree Classifier for DS2: ', "{0:.3%}".format(accuracy))
 print('\n')
 
 with open('ds2Val-dt.csv', 'w') as file:
@@ -230,12 +143,28 @@ validationPredicted = classifier.predict(validationFeaturesArray)
 validationPredictedArray = np.asarray(validationPredicted)
 
 # Naive Bayes Predicted Labels
-print('Predicted Labels for Naive Bayes')
-print(validationPredictedArray)
+print('Predicted Labels for Naive Bayes for DS2')
+#print(validationPredictedArray)
 accuracy = accuracy_score(validationLabels, validationPredictedArray)
-print('Accuracy of Naive Bayes Classifier: ', "{0:.3%}".format(accuracy))
+print('Accuracy of Naive Bayes Classifier for DS2: ', "{0:.3%}".format(accuracy))
 print('\n')
 
 with open('ds2Val-nb.csv', 'w') as file:
     for i in range(len(validationPredictedArray)):
         file.write('%d,%d\n' % (i + 1, validationPredictedArray[i]))
+
+#Linear Model Implementation
+classifier = linear_model.LinearRegression()
+classifier.fit(trainFeaturesArray, trainLabelsArray)
+validationPredicted = classifier.predict(validationFeaturesArray)
+validationPredictedArray = np.asarray(validationPredicted)
+
+#Predicted Linear Model Labels
+print('Predicted Labels for Linear Model for DS2')
+#print(validationPredictedArray)
+print('Accuracy of Linear Model Classifier for DS2: ', "{0:.3%}".format(accuracy))
+print('\n')
+
+with open('ds2Val-lm.csv', 'w') as file:
+ for i in range(len(validationPredictedArray)):
+    file.write('%d,%d\n' % (i + 1, validationPredictedArray[i]))
